848619a4dba2ea165f18f442bb4c39b16da063fb
Imene Fadhlaoui <imenefadhlaoui19@gmail.com>
2026-03-05 16:50:01 +0100

    Exercice ngStyle

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { First } from './first/first';
 import { Cv } from './cv/cv';
 import { HomeAccount } from './accounts/home-account/home-account';
+import { Direct } from './direct/direct';
 
 @Component({
   selector: 'app-root',
-  imports: [HomeAccount],
+  imports: [First,Cv,HomeAccount,Direct],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/direct/direct.ts
@@ -0,0 +1,24 @@
+import { NgClass, NgStyle } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-direct',
+  imports: [NgStyle, NgClass],
+  templateUrl: './direct.html',
+  styleUrl: './direct.css',
+})
+export class Direct {
+  bgColor = "pink";
+  txtColor = "blue";
+  font = "phosphate";
+
+  class1 = true;
+  class2 = true;
+  class3 = false;
+
+  updateclasses() {
+    this.class1 = false;
+    this.class3 = true;
+  }
+
+}
